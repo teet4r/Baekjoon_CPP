eefa5699c3ec2f39ca9cc362113e33c18c9425fd
teet4r <t4r@naver.com>
2024-07-31 17:49:00 +0900

    PI 추가

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define fast_io (ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL))
 #define cout_precision(x) (cout << fixed, cout.precision(x))
 #define nl '\n'
+#define PI 3.14159265358979323846
 
 #include "number.h"
 #include "matrix.h"
