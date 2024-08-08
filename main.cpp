de7a1f22306c51b04960d0a08f2602c0d47eccab
huijae <t4r@naver.com>
2024-08-08 09:26:41 +0900

    #define nl 삭제

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 #define fast_io (ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL))
 #define cout_precision(x) (cout << fixed, cout.precision(x))
-#define nl '\n'
 #define PI 3.14159265358979323846
 
 #include "number.h"
